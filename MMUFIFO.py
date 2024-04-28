4cf3fbf5be6d96fbf1aed36febeaecec8ae5771c
Gravehg <jeffleivajr@gmail.com>
2024-04-28 17:41:55 -0600

    Updated MMUFIFO
    Changed management of memory segments to use a pool

--- MMUFIFO.py
+++ MMUFIFO.py
@@ -8,19 +8,26 @@ from Page import Page
 class MMUFIFO():
     def __init__(self):
         #This is in kb
+        #It should be 400, 40 for testing purposes
         self.RAM = 40
         self.PAGE_SIZE  = 4
-        self.current_memory = 0
+        self.FRAME_NUM = self.RAM // 4
+        self.available_addresses = [i * self.PAGE_SIZE for i in range(self.FRAME_NUM)]
+        self.current_memory_usage = 0
         self.pointer_id_generator = 0
         self.page_id_generator = 0
         self.pointer_page_map = {}
         self.processes = []
         self.fifo_queue = []
     
+    def allocate_page(self):
+        if not self.available_addresses:
+            return None  # No available pages left
+        return self.available_addresses.pop(0)
+
     def process_new_command(self,pid,size):
         kb_size = size / 1000
         num_pages = math.ceil(kb_size / 4)
-        print("Num pages: ",num_pages)
         #Gets the process if it already exists, if not, then returns a new one
         process = self.get_process(pid)
         #Creates the new pointer
@@ -29,31 +36,70 @@ class MMUFIFO():
         process.add_pointer(new_pointer)
         for i in range(0,num_pages):
             self.page_id_generator += 1
-            if self.current_memory + self.PAGE_SIZE > self.RAM:
+            frame_address = self.allocate_page()
+            if frame_address is None:
                 memory_segment = self.replace_page()
                 new_page = Page(memory_segment,self.page_id_generator)
             else:
-                self.current_memory += self.PAGE_SIZE
-                new_page = Page(self.current_memory,self.page_id_generator)
+                self.current_memory_usage += self.PAGE_SIZE
+                new_page = Page(frame_address,self.page_id_generator)
             self.pointer_page_map[self.pointer_id_generator].append(new_page)
             self.fifo_queue.append(new_page)
     
     def process_use_command(self, pointer_id):
-        process = get_process_by_pointer(pointer_id)
+        if self.is_pointer_in_map(pointer_id):
+            pages = self.pointer_page_map[pointer_id]
+            for page in pages:
+                if not page.in_ram:
+                    frame_address = self.allocate_page()
+                    if frame_address is None:
+                        page.set_segment(self.replace_page())
+                    else:
+                        page.set_segment(frame_address)
+                    page.set_in_ram()
+                    self.fifo_queue.append(page)
+        else:
+            raise Exception("Couldn't find pointer")
+
 
     def process_delete_command(self,pointer_id):
-        process = get_process_by_pointer(pointer_id)
+        if self.is_pointer_in_map(pointer_id):
+            pages = self.pointer_page_map[pointer_id]
+            for page in pages:
+                self.delete_pages_from_queue(page)
+            del self.pointer_page_map[pointer_id]
+        else:
+            raise Exception("Couldn't find pointer")
 
     def process_kill_command(self,pid):
-        process = self.processes.remove(pid)
+        process = self.get_process_by_pid(pid)
+        self.processes.remove(process)
         pointers = process.get_pointers()
         for pointer in pointers:
-            del self.pointer_page_map[pointer.get_pointer_id()]
+            self.process_delete_command(pointer.get_pointer_id())
+
+    def is_pointer_in_map(self,pointer_id):
+        return pointer_id in self.pointer_page_map
+
+    def get_process_by_pid(self,pid):
+        for proc in self.processes:
+            if proc.get_process_id() == pid:
+                return proc
+        raise Exception("Couldn't find process")
+
+    #no se si hay que usar esto, porque la cosa es que si borro estas paginas directamente
+    #Entonces se me van a borrar ciertos segmentos de memoria y se van a perder las direcciones
+    def delete_pages_from_queue(self,page):
+        for i in self.fifo_queue:
+            if i.get_page_id() == page.get_page_id():
+                self.available_addresses.append(page.get_segment())
+                self.fifo_queue.remove(i)
+                self.current_memory_usage -= self.PAGE_SIZE
 
     
     def get_process_by_pointer(self, pointer_id):
-        for proc in self.process:
-            if is_pointer_process(pointer_id):
+        for proc in self.processes:
+            if proc.is_pointer_process(pointer_id):
                 return proc
         raise Exception("Coudln't find pointer")
     
@@ -96,3 +142,6 @@ class MMUFIFO():
     def print_queue(self):
         for i in self.fifo_queue:
             i.print_page()
+
+    def print_available_addresses(self):
+        print(self.available_addresses)
--- Page.py
+++ Page.py
@@ -7,8 +7,17 @@ class Page():
     def set_in_ram(self):
         self.in_ram = not self.in_ram
 
+    def set_segment(self,segment):
+        self.segment = segment
+
     def get_segment(self):
         return self.segment
+    
+    def get_page_id(self):
+        return self.id
+
+    def in_ram(self):
+        return self.in_ram
 
     def print_page(self):
         print(self.id, self.segment, self.in_ram)
